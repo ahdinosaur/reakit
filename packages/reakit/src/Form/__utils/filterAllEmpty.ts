ac0f28cb069b5a6e841f95825b0607904658faed
Mikey <michael.williams@enspiral.com>
2019-08-20 16:57:05 -0700

    fix bug where File field is object-ified
    before this change, if a field corresponds to a `File` object, then `onSubmit`, the field will become object-ified via `filterAllEmpty`.

--- packages/reakit/src/Form/__utils/filterAllEmpty.ts
+++ packages/reakit/src/Form/__utils/filterAllEmpty.ts
@@ -1,11 +1,11 @@
-import { isObject } from "reakit-utils/isObject";
+// import { isObject } from "reakit-utils/isObject";
 
 export function filterAllEmpty<T extends Record<any, any> | Array<any>>(
   object: T
 ): T {
   if (Array.isArray(object)) {
     return object.filter(value => {
-      if (isObject(value)) {
+      if (isPlainObject(value)) {
         return filterAllEmpty(value);
       }
       return true;
@@ -18,8 +18,26 @@ export function filterAllEmpty<T extends Record<any, any> | Array<any>>(
   for (const key of keys) {
     const k = key as keyof T;
     const value = object[k];
-    result[k] = isObject(value) ? filterAllEmpty(value) : object[k];
+    result[k] = isPlainObject(value) ? filterAllEmpty(value) : object[k];
   }
 
   return result;
+}
+
+const getPrototype = Object.getPrototypeOf
+const objectCtorString = Object.toString()
+
+// Based on lodash/isPlainObject
+function isPlainObject(val: any) {
+  if (Object.prototype.toString.call(val).slice(8, -1) !== "Object") {
+    return false
+  }
+
+  const proto = getPrototype(val)
+
+  if (proto == null) {
+    return true
+  }
+
+  return proto.constructor && proto.constructor.toString() === objectCtorString
 }
